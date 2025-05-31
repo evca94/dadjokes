f1460503288c81445f4227fd406e5348473755ea
Evca94 <evca.94@seznam.cz>
2025-05-31 19:33:09 +0200

    změna v .js

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,17 @@
-import { createRoot } from 'react-dom/client';
-import { HomePage } from './pages/HomePage';
-import './global.css';
+let upLikes = 0;
+let downLikes = 0;
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+const btnUp = document.querySelector('#btn-up');
+const btnDown = document.querySelector('#btn-down');
+
+btnUp.addEventListener('click', () => {
+  upLikes += 1;
+  const likesUpElm = document.querySelector('#likes-up');
+  likesUpElm.textContent = upLikes;
+});
+
+btnDown.addEventListener('click', () => {
+  downLikes += 1;
+  const likesDownElm = document.querySelector('#likes-down');
+  likesDownElm.textContent = downLikes;
+});
